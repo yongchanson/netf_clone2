c7d2e61c939be5d1f364d39c337d92dd098d0da8
yongchanson <syc9430@gmail.com>
2021-12-02 20:42:04 +0900

    test

--- second/src/index.js
+++ second/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './home.css';
-import App from './APP';
+import Home from './home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
---
+++ second/src/profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Header from '../components/Header';
+
+function ProfilePage() {
+  return (
+    <>
+      <Header />
+      <div>프로필 페이지</div>;
+    </>
+  );
+} 
+
+export default ProfilePage;
+
